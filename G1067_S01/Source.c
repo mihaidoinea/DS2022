40d518da74f12392dc4c062611d2622ea3a713b4
Mihai Doinea <mihai.doinea@oracle.com>
2022-03-08 08:12:56 +0200

    G1067_S03

--- G1067_S01/Source.c
+++ G1067_S01/Source.c
@@ -1,13 +1,7 @@
 #include "stdlib.h"
 int c = 4;
-void switchints (int* x, int* y, int* v1, int* v3)
-{
-	int z = *x;
-	*x = *y;
-	*y = z;
-	v1[3] = 9;
-	v3[3] = 9;
-}
+void switchints(int*, int*);
+void changeArrays(int*, int*);
 void main()
 {
 	int a = 2; 
@@ -16,14 +10,31 @@ void main()
 	//switchints(&a, &b, NULL, NULL);
 
 	int v1[] = { 1,2,3,4,5 };
-	int* v2 = { &a };
-	int* v3 = NULL;
-	v3 = (int*)malloc(5*sizeof(int));
-	v3[0] = 1;
-	v3[1] = 2;
-	v3[2] = 3;
-	v3[3] = 4;
-	v3[4] = 5;
-	switchints(&a, &b, v1, v3);
+	int* v2 = NULL;
+	v2 = (int*)malloc(5*sizeof(int));
+	v2[0] = 1;
+	v2[1] = 2;
+	v2[2] = 3;
+	v2[3] = 4;
+	v2[4] = 5;
+	switchints(&a, &b);
 	b += c;
+	int* v3 = { &v1[2] };
+	int* v4 = { &v2[3] };
+	int* v5 = { &a };
+	changeArrays(v3, v4);
+}
+//float -> double // short -> int //
+void switchints(int* x, int* y)
+{
+	int z = *x;
+	*x = *y;
+	*y = z;
+}
+
+void changeArrays(int* x, int* y)
+{
+	int* aux = x;
+	x = y;
+	y = aux;
 }
