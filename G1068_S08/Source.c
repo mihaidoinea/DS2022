ef2169526761ed01c19a762c681976c9fb35f02b
Mihai Doinea <mihai.doinea@oracle.com>
2022-04-18 13:10:38 +0300

    G1068_S08

--- G1068_S08/Source.c
+++ G1068_S08/Source.c
@@ -3,6 +3,7 @@
 #include "stdlib.h"
 #include "memory.h"
 #include "string.h"
+#include "stdbool.h"
 struct Employee
 {
 	short code;
@@ -18,18 +19,26 @@ typedef struct node
 }Node;
 
 typedef struct Employee NodeInfo;
-typedef struct node Queue;
+typedef struct queue
+{
+	Node* head;
+	Node* tail;
+}Queue;
 
 #define LINE_BUFFEER 1024
 /*functions signatures for memory management*/
 NodeInfo* createInfo(short, char*, char*, double);
 Node* createNode(NodeInfo*);
 /*functions signatures for list operations*/
-
+void put(Queue* queue, NodeInfo* info);
+bool isEmpty(Queue queue);
+NodeInfo* get(Queue* queue);
+void printInfo(NodeInfo* info);
+void deleteQueue(Queue* queue);
 
 void main()
 {
-	Queue* queue = NULL;
+	Queue queue = { .head = NULL, .tail = NULL };
 
 	FILE* pFile = fopen("Data.txt", "r");
 	char* token = NULL, lineBuffer[LINE_BUFFEER], * sepList = ",\n";
@@ -46,11 +55,61 @@ void main()
 			salary = atof(token);
 
 			NodeInfo* info = createInfo(code, name, dept, salary);
-
+			put(&queue, info);
 		}
+
+		NodeInfo* info = get(&queue);
+		printInfo(info);
+
+		deleteQueue(&queue);
 
 	}
 }
+
+void deleteQueue(Queue* queue)
+{
+	while (!isEmpty(*queue))
+	{
+		NodeInfo* info = get(queue);
+		printInfo(info);
+		free(info->dept);
+		free(info->name);
+		free(info);
+
+	}
+}
+bool isEmpty(Queue queue)
+{
+	return (queue.head == NULL && queue.tail == NULL);
+}
+NodeInfo* get(Queue* queue)
+{
+	NodeInfo* value = NULL;
+	if (!isEmpty(*queue))
+	{
+		value = queue->head->info;
+		Node* tmp = queue->head;
+		if (queue->head == queue->tail)
+			queue->head = queue->tail = NULL;
+		else
+			queue->head = tmp->pNext;
+		free(tmp);
+	}
+	return value;
+}
+
+void put(Queue* queue, NodeInfo* info)
+{
+	Node* node = createNode(info);
+	if (isEmpty(*queue))
+		queue->head = queue->tail = node;
+	else
+	{
+		queue->tail->pNext = node;
+		queue->tail = node;
+	}
+}
+
 
 void printInfo(NodeInfo* info)
 {
